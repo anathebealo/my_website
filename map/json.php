bc024bb5a3e99b0d0e493b9f780936f39d194287
anathebealo <ana.bealo@gmail.com>
2016-09-09 16:31:40 -0400

    added automatic json creation with php file that gets all info from database. TODO: edit javascript to follow pattern too

---
+++ map/json.php
@@ -0,0 +1,59 @@
+<?php
+	$configFile = fopen("configuration.txt", "r") or die("unable to open file");
+	$servername = trim(fgets($configFile));
+	$username = trim(fgets($configFile));
+	$password = trim(fgets($configFile));
+	$database = trim(fgets($configFile));
+	fclose($configFile);
+
+	try {
+	    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+	    
+		$statement = $conn->prepare("SELECT * FROM locations NATURAL JOIN (SELECT * FROM markers NATURAL JOIN markerAssignments) AS marks;");
+		$statement->execute();
+		$featuresArray = array();
+		$name = "";
+		$markerColor = "";
+		$marker = "";
+		$line = "";
+		$desc = "";
+		$lat = 0;
+		$long = 0;
+
+		while($row = $statement->fetch()) {
+			$name = $row['name'];
+			$markerColor = $row['color'];
+			$marker = $row=['markerName'];
+			$line = $row['lineColor'];
+			$desc = $row['description'];
+			$lat = $row['latitude'];
+			$long = $row['longitude'];
+
+			$propertyArray = array("name" => $name, "marker-color" => $markerColor, "marker-symbol" => $marker, "line" => $line, "popupContent" => $desc);
+			$geometryArray = array('type' => 'Point', 'coordinates' => array($long, $lat));
+			$tempArray = array('type' => 'Feature', 'properties' => $propertyArray, 'geometry' => $geometryArray);
+
+			array_push($featuresArray, $tempArray);
+		}
+
+		$data = array(
+		    'type' => 'FeatureCollection',
+		    'crs' => array(
+		    	'type' => 'name',
+		    	'properties' => array(
+		    		'name' => 'urn:ogc:def:crs:OGC:1.3:CRS84'
+		    	)
+			),
+			'features' => $featuresArray
+		);
+
+		echo json_encode($data);
+
+	    // set the PDO error mode to exception
+	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+	} catch(PDOException $e) {
+	    echo "Connection failed: " . $e->getMessage();
+	}
+
+	$conn = null;
+?>
